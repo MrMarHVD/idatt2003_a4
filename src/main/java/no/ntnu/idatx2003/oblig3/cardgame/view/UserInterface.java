4a5291d3dd9916ad1bcb5ad71a50a6a2613ac25d
Håvard Daleng <142524365+MrMarHVD@users.noreply.github.com>
2024-03-14 11:06:01 +0100

    Added images of playing cards to resources

--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/view/UserInterface.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/view/UserInterface.java
@@ -3,8 +3,10 @@ package no.ntnu.idatx2003.oblig3.cardgame.view;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -13,6 +15,27 @@ import javafx.stage.Stage;
  */
 public class UserInterface extends Application {
 
+  VBox cardDisplay;
+
+  GridPane cardPane;
+
+  TextField facesSum;
+
+  TextField cardsOfHearts;
+
+  Label flush;
+
+  Label queenOfSpades;
+
+  Button dealHand;
+
+  Button checkHand;
+
+
+  /**
+   * Start method for the application and UI.
+   * @param primaryStage the stage
+   */
   @Override
   public void start(Stage primaryStage) {
     primaryStage.setTitle("Card Game");
@@ -22,13 +45,45 @@ public class UserInterface extends Application {
     Label label = new Label("Display");
 
     VBox vbox = new VBox(textField, button, label);
-    Scene scene = new Scene(vbox, 200, 200);
+
+    int sceneWidth = 900;
+    int sceneHeight = 600;
+    Scene scene = new Scene(vbox, sceneWidth, sceneHeight);
 
     primaryStage.setScene(scene);
     primaryStage.show();
   }
 
+  /**
+   * Main method for the application.
+   *
+   * @param args the arguments
+   */
   public static void main(String[] args) {
     launch(args);
   }
+
+  /**
+   * Create and set the bounds and properties of the UI.
+   */
+  private void createUI() {
+    facesSum = new TextField();
+    cardsOfHearts = new TextField();
+    flush = new Label("-fx-padding: 10;" +
+                        "-fx-border-style: solid inside;" +
+                        "-fx-border-width: 2;" +
+                        "-fx-border-insets: 5;" +
+                        "-fx-border-radius: 5;" +
+                        "-fx-border-color: blue;");
+    queenOfSpades = new Label("-fx-padding: 10;" +
+                        "-fx-border-style: solid inside;" +
+                        "-fx-border-width: 2;" +
+                        "-fx-border-insets: 5;" +
+                        "-fx-border-radius: 5;" +
+                        "-fx-border-color: blue;");
+    dealHand = new Button("Deal Hand");
+    checkHand = new Button("Check Hand");
+  }
+
+
 }
