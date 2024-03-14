67796be6a577a01708a78b64e14b17a2292c3351
Håvard Daleng <142524365+MrMarHVD@users.noreply.github.com>
2024-03-14 11:44:19 +0100

    Improved appearance of the UI

--- src/main/java/no/ntnu/idatx2003/oblig3/cardgame/view/UserInterface.java
+++ src/main/java/no/ntnu/idatx2003/oblig3/cardgame/view/UserInterface.java
@@ -1,6 +1,7 @@
 package no.ntnu.idatx2003.oblig3.cardgame.view;
 
 import javafx.application.Application;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -8,14 +9,21 @@ import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import no.ntnu.idatx2003.oblig3.cardgame.controller.DisplayManager;
+import javafx.scene.control.Separator;
 
 /**
  * Class representing the user interface for the card game.
  */
 public class UserInterface extends Application {
+
+  /**
+   * The scene for the user interface.
+   */
+  private Scene scene;
 
   /**
    * Display Manager for this user interface.
@@ -92,7 +100,6 @@ public class UserInterface extends Application {
     primaryStage.setTitle("Card Game");
 
     this.createUI();
-    Scene scene = new Scene(UiDisplay, 800, 600);
 
     primaryStage.setScene(scene);
     primaryStage.show();
@@ -111,18 +118,72 @@ public class UserInterface extends Application {
    * Create and set the bounds and properties of the UI.
    */
   private void createUI() {
+
+    /* Initialise the components */
+
+    // Sum of faces
     facesSum = new Label("25");
+    facesSum.setMinWidth(30);
+    Label facesSumLabel = new Label("Sum of Faces: ");
+    HBox facesSumBox = new HBox(facesSumLabel, facesSum);
+
+    // Cards of hearts
     cardsOfHearts = new Label("H1");
+    cardsOfHearts.setMinWidth(30);
+    Label cardsOfHeartsLabel = new Label("Cards of Hearts: ");
+    HBox cardsOfHeartsBox = new HBox(cardsOfHeartsLabel, cardsOfHearts);
+
+    // Flush
     flush = new Label("No");
+    flush.setMinWidth(30);
+    Label flushLabel = new Label("Flush: ");
+    HBox flushBox = new HBox(flushLabel, flush);
+
+    // Queen of spades
     queenOfSpades = new Label("No");
+    queenOfSpades.setMinWidth(30);
+    Label queenOfSpadesLabel = new Label("Queen of Spades: ");
+    HBox queenOfSpadesBox = new HBox(queenOfSpadesLabel, queenOfSpades);
+
+    // Other components
     dealHand = new Button("Deal Hand");
     checkHand = new Button("Check Hand");
     cardPane = new GridPane();
     cardDisplay = new VBox(cardPane);
-    lowerLeftDisplay = new HBox(facesSum, cardsOfHearts, flush, queenOfSpades);
+    lowerLeftDisplay = new HBox(facesSumBox, cardsOfHeartsBox, flushBox, queenOfSpadesBox);
     lowerRightDisplay = new VBox(dealHand, checkHand);
-    lowerDisplay =  new HBox(lowerLeftDisplay, lowerRightDisplay);
+    Separator lowerSeparator = new Separator();
+    lowerSeparator.setOrientation(Orientation.VERTICAL);
+    lowerDisplay =  new HBox(lowerLeftDisplay, lowerSeparator, lowerRightDisplay);
     UiDisplay = new VBox(cardDisplay, lowerDisplay);
+
+    // Initialise the scene
+    scene = new Scene(UiDisplay, 800, 600);
+
+    /* Set properties for the components */
+    cardDisplay.prefHeightProperty().bind(scene.heightProperty().multiply(0.6));
+
+    HBox.setHgrow(facesSumBox, Priority.ALWAYS);
+    HBox.setHgrow(cardsOfHeartsBox, Priority.ALWAYS);
+    HBox.setHgrow(flushBox, Priority.ALWAYS);
+    HBox.setHgrow(queenOfSpadesBox, Priority.ALWAYS);
+    String labelOutlineStyle = "-fx-border-color: black; -fx-border-width: 1;";
+
+    // Add an outline to each label
+    String outlineStyle = "-fx-border-color: black; -fx-border-width: 1;";
+    String fontSize = "-fx-font-size: 16;";
+
+    facesSum.setStyle(outlineStyle + fontSize);
+    cardsOfHearts.setStyle(outlineStyle + fontSize);
+    flush.setStyle(outlineStyle + fontSize);
+    queenOfSpades.setStyle(outlineStyle + fontSize);
+    facesSumLabel.setStyle(outlineStyle + fontSize);
+    cardsOfHeartsLabel.setStyle(outlineStyle + fontSize);
+    flushLabel.setStyle(outlineStyle + fontSize);
+    queenOfSpadesLabel.setStyle(outlineStyle + fontSize);
+
+    lowerLeftDisplay.setSpacing(scene.widthProperty().multiply(0.1).doubleValue());
+    lowerRightDisplay.setSpacing(scene.heightProperty().multiply(0.1).doubleValue());
   }
 
 
